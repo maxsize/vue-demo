fa95b0a9de39eecb8f3f908cc0f7e0bc9c2aaf49
Max <maxsizen@gmail.com>
2018-04-11 00:25:58 +0800

    speech test

--- src/components/SpeechAPI.vue
+++ src/components/SpeechAPI.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     <span>Forbidden city</span><br>
-    <button>Demostrate</button><br>
+    <button @click="handleFetch">Demostrate</button><br>
     <pre></pre>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SpeechAPI'
+  name: 'SpeechAPI',
+  methods: {
+    handleFetch () {
+      const fileUri = 'gs://guidy-app.appspot.com/audio/forbidden-city.flac'
+      // const req = new Request('https://guidy-app.appspot.com/speech')
+      const req = new Request('http://localhost:8081/speech')
+      fetch(req, {
+        method: 'POST',
+        body: fileUri,
+        headers: new Headers({'Content-Type': 'application/text'})
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
