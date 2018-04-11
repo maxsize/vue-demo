6cc34a796320ab59855dcc7929965adb44ff15c3
Max <maxsizen@gmail.com>
2018-04-11 19:03:47 +0800

    send config

--- src/components/SpeechAPI.vue
+++ src/components/SpeechAPI.vue
@@ -11,12 +11,21 @@ export default {
   name: 'SpeechAPI',
   methods: {
     handleFetch () {
-      const fileUri = 'gs://guidy-app.appspot.com/audio/forbidden-city.flac'
+      const fileUri = 'gs://guidy-app.appspot.com/audio/audio.raw'
+      const encoding = 'LINEAR16';
+      const sampleRateHertz = 16000;
+      const languageCode = 'zh-CN';
       // const req = new Request('https://guidy-app.appspot.com/speech')
       const req = new Request('http://localhost:8081/speech')
+      const body = {
+        uri: fileUri,
+        encoding: encoding,
+        sampleRateHertz: sampleRateHertz,
+        languageCode: languageCode
+      }
       fetch(req, {
         method: 'POST',
-        body: fileUri,
+        body: JSON.stringify(body),
         headers: new Headers({'Content-Type': 'application/text'})
       }).then(res => {
         console.log(res)
